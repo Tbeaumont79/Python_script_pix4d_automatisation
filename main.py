6a58ed5ca821d53d0b38ee61cec3c98343d0d9e2
tbeaumont <tbeaumont79@icloud.com>
2019-05-28 13:20:29 -0400

    fixing a condition

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from isProjectAlreadyCreated import *
 
 def     main():
     projectName = 'FlightA.p4d'
-    projectNameFullPath = '\\\\nt-nas\\CDRIN\Projets\\1718_67_CGM-auto\\source_acquisition\\FLIGHT 14 A\DRONE'+projectName
+    projectNameFullPath = '\\\\nt-nas\\CDRIN\Projets\\1718_67_CGM-auto\\source_acquisition\\FLIGHT 14 A\\DRONE'+projectName
     template = 'step123'
-    projectImagePath = '\\\\nt-nas\\CDRIN\Projets\\1718_67_CGM-auto\\source_acquisition\\FLIGHT 14 A\DRONE'
-    if (!isProjectAlreadyCreated(projectNameFullPath)): 
+    projectImagePath = '\\\\nt-nas\\CDRIN\Projets\\1718_67_CGM-auto\\source_acquisition\\FLIGHT 14 A\\DRONE'
+    if (isProjectAlreadyCreated(projectNameFullPath) == False): 
         setUpNewProject(projectImagePath, projectName, template)
     else:
         print('Warning project is already build, you may change the name of the project')
