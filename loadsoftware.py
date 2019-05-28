75f0e5ef02b8d1acbaace9558917ff96dd6c4a88
tbeaumont <tbeaumont79@icloud.com>
2019-05-28 11:31:43 -0400

    fixing bug again import and path

--- loadsoftware.py
+++ loadsoftware.py
@@ -1,6 +1,7 @@
 #-------------------------#
 import os
+from newpath import setNewPath
 
-def openProjet(projectName):
+def openProjet(projectNameFullPath):
     setNewPath('C:\\Program Files\\Pix4Dmapper')
-    os.system('./pix4dmapper.exe {0}'.format(projectName))
+    os.system('./pix4dmapper.exe {0}'.format(projectNameFullPath))
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #-------------------------#
 from createProject import *
-from newpath import setNewPath
 from openfile import openFile
 from loadsoftware import *
 #-------------------------#
 
 def     main():
     projectName = 'FlightA.p4d'
+    projectNameFullPath = '\\\\nt-nas\\CDRIN\Projets\\1718_67_CGM-auto\\source_acquisition\\FLIGHT 14 A\DRONE'+projectName
     template = 'step123'
-    projectImagePath = '\\nt-nas\CDRIN\Projets\\1718_67_CGM-auto\source_acquisition\FLIGHT 14 A\DRONE​'
+    projectImagePath = '\\\\nt-nas\\CDRIN\Projets\\1718_67_CGM-auto\\source_acquisition\\FLIGHT 14 A\DRONE'
     setUpNewProject(projectImagePath, projectName, template)
-    openProjet(projectName)
+    openProjet(projectNameFullPath)
 
 if __name__ == '__main__':
     main()
