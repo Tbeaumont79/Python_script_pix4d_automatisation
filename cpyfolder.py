0570e0c05e98a5a59abe35341c6206743e75200e
tbeaumont <tbeaumont79@icloud.com>
2019-05-28 14:53:49 -0400

    adding cpyfolder script

---
+++ cpyfolder.py
@@ -0,0 +1,11 @@
+import shutil
+ 
+def copyDirectory(src, dest):
+    try:
+        shutil.copytree(src, dest)
+    # Directories are the same
+    except shutil.Error as e:
+        print('Directory not copied. Error: %s' % e)
+    # Any error saying that the directory doesn't exist
+    except OSError as e:
+        print('Directory not copied. Error: %s' % e)
