067f0e06bea071cdc717416d2f663462a6dad3c8
tbeaumont <tbeaumont79@icloud.com>
2019-06-04 11:09:31 -0400

    adding a new script that check if the project is already build

---
+++ isProjectAlreadyBuild.py
@@ -0,0 +1,26 @@
+import os
+import glob
+# this function check is the fullprojectnamepath is already created
+# return true if the project exist false if not
+def     getSize(startPath):
+    totalSize = 0
+    for dirpath, dirnames, filenames in os.walk(startPath):
+        for f in filenames:
+            filepath = os.path.join(dirpath, f)
+            totalSize += os.path.getsize(filepath)
+    return totalSize
+
+def     isProjectAlreadyBuild(projectName, startPath):
+    # first check the size of the directory
+    # second (optional) check the number of element in the directory.
+    numberOfElement = len(glob.glob(projectNameFullPath+'\*'))
+    getSize = getSize(startPath);
+    if (numberOfElement > 0 and getSize > 0):
+        return False
+    else:
+        return True
+
+def main():
+    projectName = 'FlightA';
+    projectNameFullPath = '\\\\nt-nas\\CDRIN\Projets\\1718_67_CGM-auto\\source_acquisition\\FLIGHT 14 A\\DRONE\\'+projectName
+    isProjectAlreadyBuild(projectName,projectNameFullPath)
--- isProjectAlreadyCreated.py
+++ isProjectAlreadyCreated.py
@@ -2,25 +2,6 @@ import os
 import glob
 # this function check is the fullprojectnamepath is already created
 # return true if the project exist false if not
-def     getSize(startPath):
-    totalSize = 0
-    for dirpath, dirnames, filenames in os.walk(startPath):
-        for f in filenames:
-            filepath = os.path.join(dirpath, f)
-            totalSize += os.path.getsize(filepath)
-    return totalSize
 
 def     isProjectAlreadyCreated(projectNameFullPath):
     return os.path.isfile(projectNameFullPath)
-
-def     isProjectAlreadyBuild(projectNameFullPath, startPath):
-    # first check the size of the directory
-    # second (optional) check the number of element in the directory.
-    numberOfElement = len(glob.glob(projectNameFullPath+'\*'))
-    getSize = getSize(startPath);
-    if (numberOfElement > 0 and getSize > 0):
-        return False
-    else:
-        return True
-
-
